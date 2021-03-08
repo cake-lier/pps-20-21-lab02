227dcda3103892edd0a92c7aac642e1773e04a90
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2021-03-08 16:38:23 +0100

    Refactor test code to explicit constants

--- test/lab/ShapeTest.scala
+++ test/lab/ShapeTest.scala
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test
 class ShapeTest {
     @Test
     def testRectangle(): Unit = {
-        val rect = Rectangle(2, 3)
-        assertEquals(10, perimeter(rect))
-        assertEquals(6, area(rect))
+        val rectangleBase = 2
+        val rectangleHeight = 3
+        val rectangle = Rectangle(rectangleBase, rectangleHeight)
+        assertEquals(rectangleBase * 2 + rectangleHeight * 2, perimeter(rectangle))
+        assertEquals(rectangleBase * rectangleHeight, area(rectangle))
     }
 }
