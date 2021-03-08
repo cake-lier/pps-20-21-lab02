2da355029fadcf17cbb84c20d14e1fb1eef70652
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2021-03-08 14:13:16 +0100

    Refactor fibonacci function to be tail recursive

--- src/lab/Recursion.scala
+++ src/lab/Recursion.scala
@@ -1,9 +1,15 @@
 package lab
 
+import scala.annotation.tailrec
+
 object Recursion {
-    def fib(n: Int): Int = n match {
-        case 0 => 0
-        case 1 => 1
-        case _ => fib(n - 1) + fib(n - 2)
+    def fib(n: Int): Int = {
+        @tailrec
+        def _fib(n: Int, secondToLast: Int, last: Int): Int = n match {
+            case 0 => secondToLast
+            case 1 => last
+            case _ => _fib(n - 1, last, last + secondToLast)
+        }
+        _fib(n, 0, 1)
     }
 }
