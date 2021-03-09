a08016c00c760c7ec240902bf7f9ffe4727239ea
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2021-03-09 01:18:54 +0100

    Generify types in Option module

--- src/lab/Option.scala
+++ src/lab/Option.scala
@@ -5,7 +5,7 @@ object Option {
     case class None[A]() extends Option[A]
     case class Some[A](a: A) extends Option[A]
 
-    def filter[A, B >: A](option: Option[A])(predicate: B => Boolean): Option[A] = option match {
+    def filter[A](option: Option[A])(predicate: (_ >: A) => Boolean): Option[A] = option match {
         case Some(a) => if (predicate(a)) {
             Some(a)
         } else {
@@ -14,12 +14,12 @@ object Option {
         case _ => None()
     }
 
-    def map[A, B](option: Option[A])(function: A => B): Option[B] = option match {
+    def map[A, B](option: Option[A])(function: (_ >: A) => _ <: B): Option[B] = option match {
         case Some(a) => Some(function(a))
         case _ => None()
     }
 
-    def map2[A, B, C](option1: Option[A])(option2: Option[B])(bifunction: (A, B) => C): Option[C] = option1 match {
+    def map2[A, B, C](option1: Option[A])(option2: Option[B])(bifunction: (_ >: A, _ >: B) => _ <: C): Option[C] = option1 match {
         case Some(a) => option2 match {
             case Some(b) => Some(bifunction(a, b))
             case _ => None()
